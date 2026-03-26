cc05d62e922d162088245f31a8635b4a069be580
Nazish Taj <153384749+NazishTaj@users.noreply.github.com>
2026-03-27 00:33:48 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .utils import has_permission
 from datetime import date , datetime
 from .forms import ClinicScheduleForm
 import openpyxl
