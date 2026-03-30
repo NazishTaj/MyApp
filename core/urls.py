3dcee1c8a1e137f1df2f8da3f16da3245bb72beb
Nazish Taj <153384749+NazishTaj@users.noreply.github.com>
2026-03-30 20:15:41 +0530

    Fix route ordering in urls.py

--- core/urls.py
+++ core/urls.py
@@ -33,9 +33,9 @@ path(
     name="create_bill_for_patient"
 ),
 path("bill/<int:bill_id>/print/", views.print_bill, name="print_bill"),
-path('prescription/<int:id>/', views.view_prescription, name='view_prescription'),
 path("prescription/<int:id>/revise/", views.revise_prescription, name="revise_prescription"),
 path("prescription/<int:id>/print/", views.print_prescription, name="print_prescription"),
+path('prescription/<int:id>/', views.view_prescription, name='view_prescription'),
 path("enable-advanced/", views.enable_advanced_mode, name="enable_advanced"),
 path("staff/add/", views.add_staff, name="add_staff"),
 path("staff/", views.staff_list, name="staff_list"),
