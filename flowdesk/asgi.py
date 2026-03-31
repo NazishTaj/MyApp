42b6eb3810ff44b914f3d4d6ac1c26a613c53dd7
Nazish Taj <153384749+NazishTaj@users.noreply.github.com>
2026-03-31 20:17:14 +0530

    Update asgi.py

--- flowdesk/asgi.py
+++ flowdesk/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 from django.urls import path
 from core.consumers import DashboardConsumer   
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowdesk.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
