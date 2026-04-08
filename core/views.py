92cb876721fae2b4454e10c40bd2d217d3d4163f
Nazish Taj <153384749+NazishTaj@users.noreply.github.com>
2026-04-09 00:25:18 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
