0c3024fb1a84600ef60519bf98a42aaddf6bbcb9
Nazish Taj <153384749+NazishTaj@users.noreply.github.com>
2026-05-03 02:20:22 +0530

    Update asgi.py

--- flowdesk/asgi.py
+++ flowdesk/asgi.py
@@ -1,21 +1,21 @@
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack   # ✅ ADD THIS
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
-# ✅ FIRST: settings set karo
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowdesk.settings')
 
-# ✅ SECOND: Django init karo
 django_asgi_app = get_asgi_application()
 
-# ✅ THIRD: ab import karo
 from core.consumers import DashboardConsumer
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
 
-    "websocket": URLRouter([
-        path("ws/dashboard/", DashboardConsumer.as_asgi()),
-    ]),
+    "websocket": AuthMiddlewareStack(   # ✅ CHANGE HERE
+        URLRouter([
+            path("ws/dashboard/", DashboardConsumer.as_asgi()),
+        ])
+    ),
 })
